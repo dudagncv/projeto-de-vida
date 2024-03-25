c4067a607047cbed180bee72588a9924b0377974
dudagncv <125581993+dudagncv@users.noreply.github.com>
2024-03-25 11:23:32 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,4 @@
+const textos = document.querySelectorAll(“.aba-conteudo”);
+textos[i].classList.add(“ativo”);
+textos[j].classList.remove(“ativo”);
+const contadores = document.querySelectorAll(“.contador”);
